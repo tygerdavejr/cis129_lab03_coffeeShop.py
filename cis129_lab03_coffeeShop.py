7d75f39dac5fca6c720e4cd46e01aee233358e8e
David Vance <dvance4@mail.pima.edu>
2024-09-12 21:24:07 -0700

    Update cis129_lab03_coffeeShop.py
    Updated to convert pseudocode to code

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -15,8 +15,8 @@
 
 coffee_price = 5
 muffin_price = 4
-# Add Item #3
-# Add Item #4
+smoothie_price = 3 # Add Item #3
+burrito_price = 5 # Add Item #4
 
 tax_rate = .06
 
@@ -32,9 +32,9 @@ print (banner_one)
 
 print ('My Coffee and Muffin Shop')
 coffee_amount = int (input ('Number of coffees bought? \n'))
-muffin_amount = int (input('Number of muffins bought? \n'))
-#input number for Item #3
-#input number for Item #4
+muffin_amount = int (input ('Number of muffins bought? \n'))
+smoothie_amount = int (input ('Number of smoothies bought? \n')) #input number for Item #3
+burrito_amount = int (input ('Number of burritos bought? \n')) #input number for Item #4
 
 print (banner_one, ('\n'))
 
@@ -47,10 +47,11 @@ print (banner_one, ('\n'))
 
 coffee_total = coffee_price * coffee_amount
 muffin_total = muffin_price * muffin_amount
-# Determine Item 3 sales
-# Determine Item 4 sales
+smoothie_total = smoothie_price * smoothie_amount # Determine Item 3 sales
+burrito_total = burrito_price * burrito_amount # Determine Item 4 sales
 
-sales_total = coffee_total + muffin_total # update to include items 3 and 4
+# update to include items 3 and 4
+sales_total = coffee_total + muffin_total + smoothie_total + burrito_total 
 
 if coffee_amount == 1:
     coffee_label = 'Coffee'
@@ -63,9 +64,17 @@ else:
     muffin_label = 'Muffins'
 
 # Set singular or plural for Item 3
+if smoothie_amount == 1:
+    smoothie_label = 'Smoothie'
+else:
+    smoothie_label = 'Smoothies'
 
 # Set singular or plural for Item 4
-
+if burrito_amount == 1:
+    burrito_label = 'Burrito'
+else:
+    coffee_label = 'Burritos'
+    
 ##############################################################################
 # OUTPUT
 # Print the receipt in accordance with customer requirements. 
@@ -90,8 +99,8 @@ print (banner_one)
 # convert int to str to display values as requested
 coffee_price_text = '$' + str(coffee_price)
 muffin_price_text = '$' + str(muffin_price)
-# Set Item 3 Price Text
-# Set Item 4 Price Text
+smoothie_price_text = '$' + str(smoothie_price) # Set Item 3 Price Text
+burrito_price_text = '$' + str(burrito_price)# Set Item 4 Price Text
 
 sales_tax = tax_rate * sales_total
 sales_tax_text = str(sales_tax)
@@ -106,8 +115,10 @@ print (coffee_amount, coffee_label, 'at', coffee_price_text, 'each: $',
        f"{coffee_total:.2f}")
 print (muffin_amount, muffin_label, 'at', muffin_price_text, 'each: $',
        f"{muffin_total:.2f}")
-# Print Item 3 line
-# Print Item 4 line
+print (smoothie_amount, smoothie_label, 'at', smoothie_price_text, 'each: $',
+       f"{smoothie_total:.2f}")# Print Item 3 line
+print (burrito_amount, burrito_label, 'at', burrito_price_text, 'each: $',
+       f"{burrito_total:.2f}")# Print Item 4 line
 
 print (int(tax_rate * 100),'\b% tax: $', sales_tax_text) 
 
@@ -115,7 +126,7 @@ print (banner_two)
 print ('Total: $', f"{(sales_tax + sales_total):.2f}")
 
 # Print Thank You line
-
+print ('C|_| Thank you for shopping My Coffee and Muffin Shop ---<@ ')
 print (banner_one)
 
 
